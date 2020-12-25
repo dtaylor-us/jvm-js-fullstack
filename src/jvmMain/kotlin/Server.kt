fd18b8eff5ea9bcf14ed55f3cb5ad1dc987f21e2
dtaylor-us <derektaylor.us@gmail.com>
2020-12-25 05:35:09 -0600

    persistence

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,3 +1,60 @@
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.litote.kmongo.*
+import org.litote.kmongo.coroutine.*
+import org.litote.kmongo.reactivestreams.KMongo
+import com.mongodb.ConnectionString
+
+val client = KMongo.createClient().coroutine
+val database = client.getDatabase("shoppingList")
+val collection = database.getCollection<ShoppingListItem>()
+
 fun main() {
-    println("Hello, JVM!")
+    embeddedServer(Netty, 9090) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
+        routing {
+            get("/") {
+                call.respondText(
+                    this::class.java.classLoader.getResource("index.html")!!.readText(),
+                    ContentType.Text.Html
+                )
+            }
+            static("/") {
+                resources("")
+            }
+            route(ShoppingListItem.path) {
+                get {
+                    call.respond(collection.find().toList())
+                }
+                post {
+                    collection.insertOne(call.receive<ShoppingListItem>())
+                    call.respond(HttpStatusCode.OK)
+                }
+                delete("/{id}") {
+                    val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
+                    collection.deleteOne(ShoppingListItem::id eq id)
+                    call.respond(HttpStatusCode.OK)
+                }
+            }
+        }
+    }.start(wait = true)
 }
